0360b1b2849ef6af1368e975eca062c534c461c6
Brian Nguyen <briannguyen.sd@gmail.com>
2023-06-22 14:23:43 -0400

    Add rate limiting

--- server/server.js
+++ server/server.js
@@ -2,14 +2,21 @@ const spotifyWebApi = require("spotify-web-api-node");
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const rateLimit = require("express-rate-limit");
 const { CLIENT_ID, CLIENT_SECRET } = require("./config");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Limit to 2 requests per minute
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 2,
+});
+
 // Listen for a login request and send back an access token
-app.post("/login", (req, res) => {
+app.post("/login", limiter, (req, res) => {
   const code = req.body.code;
   const spotifyApi = new spotifyWebApi({
     clientId: CLIENT_ID,
